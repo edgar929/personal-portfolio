59996030ae75b2a054d44672f1bf4c37f5a14083
edgar929 <shumbushoedgar@gmail.com>
2020-11-05 23:26:55 +0200

    creating elements img,button, hr

--- js/read-article.js
+++ js/read-article.js
@@ -3,31 +3,35 @@ const art = document.querySelector('#article-detail');
 
 function renderArticle(doc){
     let div = document.querySelector('#retrieve-article');
-    let div2 = document.querySelector('#article-detail')
-    let tit = document.querySelector('#articleTitle');
-    let tit2 = document.querySelector('#articleTitles');
-    let summary = document.querySelector('#articleSummary');
-    let cont = document.querySelector('#articleContent');
-
+    let a = document.createElement('a');
+    let tit = document.createElement('h2');
+    let summary = document.createElement('p');
+    let image = document.createElement('img');
+    let btn = document.createElement('button');
+    let  hr= document.createElement('hr');
+    
     div.setAttribute('article-id', doc.id);
     tit.textContent = doc.data().title;
     summary.textContent = doc.data().summary;
-   
-
-    // article.appendChild(div.appendChild(tit));
-    // article.appendChild(div.appendChild(summary));
-   
-
-    document.querySelector('#viewMore').addEventListener('click', function(event){
-        let id =event.target.parentNode.getAttribute('article-id');
-        // console.log(id);
-        // let doc2=db.collection('articles').doc(id);
-        window.open('blog-details.html');
-    })  
+    image.textContent = '';
+    btn.textContent='view more';
+    
+    article.appendChild(div.appendChild(image));
+    a.appendChild(article.appendChild(div.appendChild(tit)));
+     article.appendChild(div.appendChild(summary));
+     article.appendChild(div.appendChild(btn));
+     article.appendChild(div.appendChild(hr));
+     
+    // document.querySelector('#viewMore').addEventListener('click', function(event){
+    //     let id =event.target.parentNode.getAttribute('article-id');
+       
+    //     window.open('blog-details.html');
+    // })  
 }
 
 db.collection('articles').get().then((snapshot)=>{
     snapshot.docs.forEach(doc=>{
      renderArticle(doc);
+     console.log(doc.data())
     })
 })
