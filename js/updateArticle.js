2493cbc88bb845af7da99a3c7e41a4b23b702ba7
edgar929 <shumbushoedgar@gmail.com>
2020-11-06 11:03:49 +0200

    populating the add-article form once clicked the update button

--- js/updateArticle.js
+++ js/updateArticle.js
@@ -1,19 +1,31 @@
-let id =window.Hash.slice(1);
+let id=location.hash.slice(1);
 
-function renderArticle(doc){
-    
-
-    //updating article
-    action2.addEventListener('click', (e)=>{
-        e.stopPropagation();
-        let id=e.target.parentElement.getAttribute('article-id');
-         window.location.href=`add-article.html#${id}`;
-    })
-
+function renderArticle(article){
+    const title=document.querySelector('#title');
+    const summary=document.querySelector('#summary');
+    const content=document.querySelector('#content');
+    title.value=article.title;
+    content.value=article.content;
+    summary.value = article.summary;
 }
 
-db.collection('articles').get().then((snapshot)=>{
-    snapshot.docs.forEach(doc=>{
-     renderArticle(doc);
+db.collection('articles').doc(id).get().then((article)=>{
+    renderArticle(article.data());
+}) 
+
+const form=document.querySelector('#new-post');
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    db.collection('posts').doc(id).update({
+        title:form.title.value,
+        content:form.content.value,
+        image:'../Image/blog1.jpg'
     })
+
+    document.querySelector('[name= title]').value=' ';
+    document.querySelector('[name= content]').value=' ';
+
+    setTimeout(()=>{
+        window.location.href='add-article.html';
+    },1500)
 })
