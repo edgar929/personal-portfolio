d8d7eef6eec3bbb594030fc032c230c6cdd3e231
edgar929 <shumbushoedgar@gmail.com>
2020-11-08 17:00:51 +0200

    adds update functionality

--- js/updateArticle.js
+++ js/updateArticle.js
@@ -13,13 +13,15 @@ db.collection('articles').doc(id).get().then((article)=>{
     renderArticle(article.data());
 }) 
 
-const form=document.querySelector('#new-post');
-form.addEventListener('submit', e =>{
+const form=document.querySelector('#addArticle');
+form.addEventListener('submit', (e) =>{
     e.preventDefault();
-    db.collection('posts').doc(id).update({
-        title:form.title.value,
-        content:form.content.value,
-        image:'../Image/blog1.jpg'
+    db.collection('articles').doc(id).update({
+        
+             content: form.content.value,
+            picture:'',
+            summary: form.summary.value,
+            title: form.title.value
     })
 
     document.querySelector('[name= title]').value=' ';
