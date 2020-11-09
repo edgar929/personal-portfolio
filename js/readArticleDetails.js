ca1100f80cf72c9eb04f512475ddf0cc89a3d119
edgar929 <shumbushoedgar@gmail.com>
2020-11-09 20:46:44 +0200

    adds comments and read them

--- js/readArticleDetails.js
+++ js/readArticleDetails.js
@@ -17,14 +17,48 @@ db.collection('articles').doc(id).get().then((article)=>{
 
 const form = document.querySelector('#commentForm');
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    const name = document.querySelector('#commentorName'); 
-    const comment = document.querySelector('#commentorComment'); 
-    db.collection('articles').doc(id).add({
-        comment:{
-            name,
-            comment
+form.addEventListener('submit', (e)=>{
+          e.preventDefault();
+         
+            db.collection('comments').add({
+              commentId: id,
+              name: form.name.value,
+              comment: form.comment.value
+              
+          })
+          form.name.value='';
+          form.comment.value='';
+          location.reload();
+          })
+
+          //reading comments
+
+          function renderComments(doc){
+            let div = document.querySelector('#comments');
+            let div2 = document.querySelector('.commentor');
+            let name = document.createElement('span')
+            let comment = document.createElement('p')
+            let image = document.createElement('img')
+            let hr = document.createElement('hr')
+
+            div.setAttribute('comment-id', doc.id);
+            name.textContent = doc.data().name;
+            comment.textContent = doc.data().comment;
+            
+            
+            // div2.appendChild(image.src =  '../images/avatar.png')
+            div.appendChild(name);
+            div.appendChild(comment)
+            div.appendChild(hr)
         }
-    })
-})
+        
+        
+        db.collection('comments').get().then((snapshot)=>{
+            
+            snapshot.docs.forEach(doc=>{
+                if(doc.data().commentId == id){
+                    renderComments(doc);
+                }
+             
+            })
+        })
