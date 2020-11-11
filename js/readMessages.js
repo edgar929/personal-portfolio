e87ca8df01911286b22bf87f37afa3d8a14d76d7
edgar929 <shumbushoedgar@gmail.com>
2020-11-11 02:05:58 +0200

    reading messages

--- js/readMessages.js
+++ js/readMessages.js
@@ -1,21 +1,32 @@
 const trow = document.querySelector('#messages');
 
 function renderMessage(doc){
-    let li = document.createElement('li');
-    let div = document.createElement('div');
-    let name = document.createElement('span');
-    let message = document.createElement('span');
-    let action1 = document.createElement('button');
-
+    // let li = document.createElement('li');
+    // let div = document.createElement('div');
+    // let name = document.createElement('span');
+    // let message = document.createElement('span');
+    // let action1 = document.createElement('button');
+    let accord = document.querySelector('.accordion');
+    let accpara = document.querySelector('#accMessage');
+    let panel = document.querySelector('.panel');
+    let div = document.querySelector('#singleMessage')
     li.setAttribute('message-id', doc.id);
     name.textContent = doc.data().fullName+' | '+doc.data().email;
     // div.textContent = doc.data().message;
     action1.textContent = 'delete';
 
-    li.appendChild(name);
-    li.appendChild(div);
-    li.appendChild(div.appendChild(action1));
-    trow.appendChild(li);
+    accord.textContent = doc.data().fullName;
+    accpara.textContent = doc.data().message;
+    
+    panel.appendChild(accpara);
+    div.appendChild(accord);
+    div.appendChild(panel)
+    
+
+    // li.appendChild(name);
+    // li.appendChild(div);
+    // li.appendChild(div.appendChild(action1));
+    // trow.appendChild(li);
     //deleting article
     action1.addEventListener('click', (e)=>{
         e.stopPropagation();
